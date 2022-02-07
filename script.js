0d17bded91a2496301916c41565516ae7dc6b007
mulfranck <mulfranck5@gmail.com>
2022-02-07 06:21:12 +0100

    The script Board
    the board is for my ability all done

---
+++ script.js
@@ -0,0 +1,43 @@
+const gameBoard = (() => {
+  let _board = ["", "", "", "", "", "", "", "", ""];
+  let $game_board = document.querySelector(".game_board");
+
+  //   Get the exact location on the board to add a selected
+  const _getField = (e) => {
+    let entered_pos = Number(e.target.attributes["data-id"].value);
+    // _board[entered_pos] = "s";
+    update_board(_board[entered_pos], "S");
+    e.target.textContent = "s"; //update the dom with the players key
+    e.target.classList.toggle("btn");
+  };
+
+  const update_board = (toUpdate, updater) => {
+    toUpdate = updater;
+  };
+  const setBoard = () => {
+    $game_board.addEventListener("click", _getField);
+  };
+
+  //   const _rest_board = () => {
+  //     for (val in _board) {
+  //       update_board((_board[val], ""));
+  //       update_board($game_board.children[val].textContent, " ");
+  //       console.log($game_board.children[val].textContent);
+  //     }
+  //   };
+
+  //   let $restart_btn = document.querySelector("#restart");
+  //   $restart_btn.addEventListener("click", _rest_board);
+  return {
+    setBoard,
+  };
+})();
+
+// const PlayerFactory = (name) => {
+//   _name = "";
+//   _scores = 0;
+//   const _setScores = (score) => (_scores = score);
+//   const _setName = () => (_name = name);
+
+//   //   what can player do? player can set his name, have a score of wins, wait for the other player,
+// };
